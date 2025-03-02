e3b429165c743626643ad4f796241893a0e29c9a
Ivan Kondzerau <konderev@aheadworks.com>
2025-03-02 15:22:04 +0300

    implement simple html layout

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css';
 
-const app = document.querySelector('#app');
-if (app) {
-  app.innerHTML = `
-    <h1 class="text-3xl font-bold">Hello world!</h1>
-    `;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const copyrightDate = document.querySelector("#copyright .date");
+  if (copyrightDate) {
+    copyrightDate.innerHTML = new Date().getFullYear().toString();
+  }
+});
