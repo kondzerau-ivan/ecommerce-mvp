51843ddfaf5cad3f634532789aaf9772ed3e07a0
Ivan Kondzerau <konderev@aheadworks.com>
2025-03-13 21:21:32 +0300

    fix: remove timeout

--- src/functions.ts
+++ src/functions.ts
@@ -2,21 +2,18 @@ import { getCustomerData } from './api';
 
 const setCustomerAccessToken = (token: string): void => {
   document.cookie = `customerAccessToken=${token}; path=/; samesite=strict`;
-}
+};
 
 const setCustomerAuthenticatedView = (loginButton: HTMLButtonElement, accountButton: HTMLAnchorElement) => {
   loginButton.classList.add('hidden');
   loginButton.disabled = false;
   accountButton.classList.remove('hidden');
-}
+};
 
-export const handleCustomerLogin = (loginButton: HTMLButtonElement, accountButton: HTMLAnchorElement): void => {
+export const handleCustomerLogin = async (loginButton: HTMLButtonElement, accountButton: HTMLAnchorElement): void => {
   loginButton.disabled = true;
-
-  setTimeout(async () => {
-    const data = await getCustomerData();
-    const token = data.accessToken;
-    setCustomerAccessToken(token);
-    setCustomerAuthenticatedView(loginButton, accountButton);
-  }, 1000);
-}
+  const data = await getCustomerData();
+  const token = data.accessToken;
+  setCustomerAccessToken(token);
+  setCustomerAuthenticatedView(loginButton, accountButton);
+};
