77f27113d7ea971cffd15a205d254256dad1dc9d
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2025-03-22 10:55:02 +0300

    feat: set custom event - login (#5)
    * feat: set custom event - login
    
    * feat: render customer image
    
    * feat: logout logic
    
    * fix: change CustomerLoginEvent name
    
    ---------
    
    Co-authored-by: Ivan Kondzerau <konderev@aheadworks.com>

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,19 +1,63 @@
 import { getCustomerData } from '../api';
+import { loginButton, accountButton, customerImage, customerGreeting } from '../selectors';
 
 const setCustomerAccessToken = (token: string): void => {
   document.cookie = `customerAccessToken=${token}; path=/; samesite=strict`;
 };
 
-const setCustomerAuthenticatedView = (loginButton: HTMLButtonElement, accountButton: HTMLAnchorElement) => {
-  loginButton.classList.add('hidden');
-  loginButton.disabled = false;
-  accountButton.classList.remove('hidden');
+const setCustomerLoginEvent = (data: object): void => {
+  const event = new CustomEvent('login:before', {
+    detail: {
+      bubbles: true,
+      cancelable: true,
+      user: data
+    }
+  });
+
+  window.dispatchEvent(event);
 };
 
-export const handleCustomerLogin = async (loginButton: HTMLButtonElement, accountButton: HTMLAnchorElement): Promise<void> => {
+const setCustomerLogoutEvent = (): void => {
+  const event = new CustomEvent('logout', {
+    detail: {
+      isLogin: false,
+      bubbles: true,
+      cancelable: true
+    }
+  });
+
+  window.dispatchEvent(event);
+};
+
+export const handleLoginButtonClick = async (): Promise<void> => {
+  loginButton.classList.add('loading');
   loginButton.disabled = true;
+
   const data = await getCustomerData();
-  const token = data.accessToken;
-  setCustomerAccessToken(token);
-  setCustomerAuthenticatedView(loginButton, accountButton);
+
+  setCustomerLoginEvent(data);
+  setCustomerAccessToken(data.accessToken);
+
+  loginButton.classList.add('hidden');
+  loginButton.classList.remove('loading');
+  accountButton.classList.remove('hidden');
+
+  
+  if (customerImage) customerImage.setAttribute('src', data.image);
+
+  if (customerGreeting) {
+    customerGreeting.innerHTML = `Hey <strong>${data.firstName}</strong>!`;
+    customerGreeting.classList.remove('hidden');
+  };
+
+};
+
+export const handleLogoutButtonClick = (): void => {
+  document.cookie = `customerAccessToken=; path=/; Max-Age=-1; samesite=strict`;
+  accountButton.classList.add('hidden');
+  loginButton.classList.remove('hidden');
+  loginButton.disabled = false;
+  customerGreeting.classList.add('hidden');
+  customerImage.setAttribute('src', '');
+  setCustomerLogoutEvent();
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { handleCustomerLogin } from './auth';
 import './style.css';
+import { handleLoginButtonClick, handleLogoutButtonClick } from './auth';
+import { loginButton, accountButton, logoutButton } from './selectors';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const loginButton = document.querySelector(".js-login") as HTMLButtonElement;
-  const accountButton = document.querySelector(".account") as HTMLAnchorElement;
-
   if (loginButton && accountButton)
-    loginButton.addEventListener('click', () => handleCustomerLogin(loginButton, accountButton));
+    loginButton.addEventListener('click', handleLoginButtonClick);
+
+  if (logoutButton) logoutButton.addEventListener('click', handleLogoutButtonClick);
+
 
   // Update the copyright date
-  const copyrightDate = document.querySelector(".js-copyright-date") as HTMLElement;
+  const copyrightDate = document.querySelector('.js-copyright-date') as HTMLElement;
   if (copyrightDate) {
     copyrightDate.innerText = new Date().getFullYear().toString();
   }
