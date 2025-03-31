35b3ef73d72665b616a2f9fb5009e1557e0cc29f
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2025-03-31 11:28:58 +0300

    Fetch products from the endpoint (#6)
    * feat: set custom event - login
    
    * feat: render customer image
    
    * feat: logout logic
    
    * fix: change CustomerLoginEvent name
    
    * feat: fetch products from the endpoint
    
    * feat: fetch products from the endpoint
    
    ---------
    
    Co-authored-by: Ivan Kondzerau <konderev@aheadworks.com>

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import { LOGIN_URL } from "./config";
+import { LOGIN_URL, GET_PRODUCTS_DATA } from "./config";
 
 interface GetCustomerTokenParams {
   username?: string;
@@ -9,6 +9,18 @@ interface GetCustomerTokenResponse {
   accessToken: string;
   image: string;
   firstName: string;
+}
+
+interface GetCustomerProductsResponse {
+  products: {
+    id: number,
+    title: string,
+    description: string,
+    images: string[]
+  };
+  total: number;
+  skip: number;
+  limit: number;
 }
 
 export const getCustomerData = async ({username = "emilys", password = "emilyspass"}: GetCustomerTokenParams = {}): Promise<GetCustomerTokenResponse> => {
@@ -23,9 +35,26 @@ export const getCustomerData = async ({username = "emilys", password = "emilyspa
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    return await response.json() as GetCustomerTokenResponse;
+    return await response.json();
   } catch (error) {
     console.error('Error fetching customer token:', error);
     throw error; // Re-throw the error after logging it
+  }
+};
+
+export const getProductsData = async (): Promise<GetCustomerProductsResponse> => {
+  const url = `${GET_PRODUCTS_DATA}?select=id,title,description,images`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json()
+    return data.products;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
   }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import './style.css';
 import { handleLoginButtonClick, handleLogoutButtonClick } from './auth';
 import { loginButton, accountButton, logoutButton } from './selectors';
+import { getProductsData } from './api';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   if (loginButton && accountButton)
+    loginButton.addEventListener('click', handleLoginButtonClick);
+
+  if (logoutButton) logoutButton.addEventListener('click', handleLogoutButtonClick);
+
     loginButton.addEventListener('click', handleLoginButtonClick);
 
   if (logoutButton) logoutButton.addEventListener('click', handleLogoutButtonClick);
@@ -14,4 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (copyrightDate) {
     copyrightDate.innerText = new Date().getFullYear().toString();
   }
+
+  const products = await getProductsData();
+  console.log(products);
 });
